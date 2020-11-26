b540cc5a5100018ac4ba9ed877cb77dc2cc3d8ba
Chen Perach <chen76273@gmail.com>
2020-11-27 00:12:04 +0200

    read me rewrite, the project is now a library

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
-# vision rewrite
+# Open Vision
+**open vision** is an attempt to ease development of vision processing code during an FRC season.
+this project is written in python and uses the ```open cv``` image processing library.  
 
-**vision rewrite** is my attempt to fix the [broken code I wrote in 2019](https://github.com/chenPerach/vision2019), my first year in frc.
-this project addes more more robust and advanced then the code for many reasons.
+as an example the code in the project solves the [FRC 2019 Deep Space](https://en.wikipedia.org/wiki/Destination:_Deep_Space) vision challange.
+![2019_image_example](assets/image_dir_example.png)
+![2019_image_example](assets/example_image.png)
 
-* calculates the distance and angles currectly and more accuratly
-* overall easier to read and less prone to bugs
-* easier to extend the capabilities of the project (because of better use of S.O.L.I.D princples)
-* has comments
+note that this project can do much more then that.
+![different image](assets/basic_example.png)
 
+## Guide
+this project requires you to have the [```open cv```](https://opencv.org) library for python
+if you don't have it just use this following command:
+
+```
+pip install python-opencv
+```
+
+note that this project uses *opencv version 4.2.0 numpy version 1.16.6 and python version 3.6.9*
+and that different version could cause unwanted errors
+
+### Cool now how do I make this project my own?
+that's easy!
+first you need to understand how to project is structured.
+``` app/processor.py ``` (yeah I know that's a bad name) handles the importent stuff, that being the *pipe line* 
+of the project.
+in the ```run``` method you can just write whatever opencv code you want and it will handle it perfectly
+
+one more thing you should note when writing code is that the function expects you to return a *map* of stuff
+I personaly put images, debug messages and the processing's results.
+that map will be used for farther development. 
+like streaming these images and slider on a local site
+ 
 ## TODO
 even though this project is much better then the last one there is still much needed work to be done.
 
@@ -15,4 +39,5 @@ even though this project is much better then the last one there is still much ne
 * add a script to change the wifi address to a static one at will
 * add a web server that allows to debug and view the image from the robot's wifi
 
+> this project started as rewrite of [this](https://github.com/chenPerach/vision2019), my first solution to the FRC 2019 vision processing challange, this was my first ever coding challange and I had fun doing it
 
--- app/processor.py
+++ app/processor.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 
 tm = 0.325
-fov = 30
+fov = math.radians(30)
 class process:
     def __init__(self,path,using_camera = True):
         self.sliders = Sliders(isGUI=True,path = path)
@@ -22,7 +22,7 @@ class process:
 
     
     def run(self):
-
+        ## write your pipeline in this following method
         debug_messages = []
         if (self.using_cam):
             if(not self.cam.is_open()):
@@ -35,6 +35,7 @@ class process:
             img = self.image_fetcher.read() 
         
         img = cv2.resize(img,(420,320),interpolation=cv2.INTER_AREA)
+
         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
 
         upper,lower = self.sliders.getHSV()
@@ -42,19 +43,25 @@ class process:
         mask_img = cv2.inRange(hsv_img,lower,upper)
         bit_img = cv2.bitwise_and(img,img,mask = mask_img)
 
-        img_width,img_height,_ = img.shape
+        img_height,img_width,_ = img.shape
+
+        cv2.line(bit_img,(int(img_width//2),0),(int(img_width//2),int(img_height)),(255,0,0),3)
 
         contours, _ = cv2.findContours(mask_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         filtered_contours = []
 
         for cnt in contours:
-            if(cv2.contourArea(cnt) > 150):
-                x,y,w,h = cv2.boundingRect(cnt)
+            area = cv2.contourArea(cnt)
+            x,y,w,h = cv2.boundingRect(cnt)
+            rect_area = w*h
+
+            if(area/rect_area > 0.4 and area > 100 and h/w > 1.2):
                 filtered_contours.append((cnt,x))        
         
         #sort the contours from left to right
-        sorted(filtered_contours,key = lambda x: x[1])
+        filtered_contours.sort(key = lambda x: x[1])
+        # sorted(filtered_contours,key = lambda x: x[1])
         filtered_contours = [cnt[0] for cnt in filtered_contours]
 
         results = []
@@ -62,7 +69,15 @@ class process:
             # find the middle y value of both rectangles
             x_1,y_1,w_1,h_1 = cv2.boundingRect(filtered_contours[i])
             x_2,y_2,w_2,h_2 = cv2.boundingRect(filtered_contours[i+1])
-            mid_y  = (y_1+y_2+(h_1+h_2)/2)/2
+            
+            cv2.rectangle(bit_img,(x_1,y_1),(x_1+w_1,y_1+h_1),(0,255,0))
+            cv2.rectangle(bit_img,(x_2,y_2),(x_2+w_2,y_2+h_2),(0,255,0))
+
+            cv2.putText(bit_img,str(i),(x_1,y_1),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0))
+            cv2.putText(bit_img,str(i+1),(x_2,y_2),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0))
+            
+            mid_y = (y_1+y_2+(h_1+h_2)/2)/2
+            mid_x = (x_1+x_2+w_1)/2
 
             #get the lines from the contors
             [vx_1,vy_1,x_1,y_1] = cv2.fitLine(filtered_contours[i], cv2.DIST_L2,0,0.01,0.01)
@@ -74,27 +89,28 @@ class process:
             collision_point = l1.find_collision(l2)
             
             # if the collision point is lower then the mid_y then go to next couple of contors
-            if collision_point.y >= mid_y: 
+            if collision_point.y >= y_1 + h_1 or collision_point.y >= y_2 + h_2  : 
                 continue
-
+            
             # draw debug stuff
-            cv2.line(bit_img,(int(collision_point.x),int(0)),(int(collision_point.x),int(img_height)),(255,0,0),2)
+            p = vector(mid_x,mid_y)
+            cv2.circle(bit_img,(int(p.x),int(p.y)),2,(0,255,0),2)
 
-            angles = measure.measure_angle(collision_point,(img_width,img_height),(fov,fov))
-            distances = measure.get_distance(vector(x_1+w_1,y_1),vector(x_2,y_2),(img_width,img_height),
+            angles = measure.measure_angle(p,(img_width,img_height),(fov,fov))
+            distances = measure.get_distance(vector(x_1+w_1,0),vector(x_2,0),(img_width,img_height),
             (fov,fov),tm,angles)
 
             results.append((angles,distances))
-
+            debug_messages.append("".join(["angle: "+str(math.degrees(angles[0]))+" distance "+str(distances[0])]))
         return {
             "images": [
                 {
-                    "title": "image",
-                    "image": img
-                },
-                {
                     "title": "bit wise image",
                     "image": bit_img    
+                },
+                {
+                    "title": "mask",
+                    "image": mask_img
                 }
             ],
             "messages": debug_messages,
