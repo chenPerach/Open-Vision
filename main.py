d199266940221d62529bb05f3854efce01e527b3
Chen Perach <chen76273@gmail.com>
2020-11-26 15:52:13 +0200

    removed small print

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from app.processor import process
 import cv2
 if __name__ == "__main__":
     path = Path.joinpath(Path.cwd(),"json","HSVdata.json")
-    process = process(path=path,using_camera=False)
+    process = process(path=path,using_camera=True)
 
     while(True):
         process.run()
-
-
 
         k = cv2.waitKey(1)
         if k == 27: # if the user wants to exit the program 
@@ -18,6 +16,5 @@ if __name__ == "__main__":
             process.image_fetcher.next()
         if k == 81:
             process.image_fetcher.prev()
-        print(k)
     process.end()
 
